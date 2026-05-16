cfaa30b54b76ffce5a3a4d8d1ccb1d519b874ca7
MashedD <sl8code@gmail.com>
2026-05-16 11:07:23 +0200

    Add sending hit info

--- g_combat.c
+++ g_combat.c
@@ -398,6 +398,14 @@ void T_Damage(edict_t *targ, edict_t *inflictor, edict_t *attacker, vec3_t dir,
 
         targ->health = targ->health - take;
 
+        // send hit marker to attacker
+        if (attacker && attacker->client && attacker != targ) {
+            gi.WriteByte(SVC_TEMP_ENTITY);
+            gi.WriteByte(TE_DAMAGE_DEALT);
+            gi.WriteShort(take);
+            gi.unicast(attacker, false);
+        }
+
         if (targ->health <= 0) {
             if ((targ->svflags & SVF_MONSTER) || (client)) {
                 targ->flags |= FL_NO_KNOCKBACK;
