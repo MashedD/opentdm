171de96dc03e44b9cdc06e3cccb7807cd972e364
MashedD <sl8code@gmail.com>
2026-05-18 13:54:32 +0200

    Add damage indicators
    Modified q2pro with "scr_damage_indicator" turned on is required.
    Similar functionality to one seen in Remaster.

--- g_local.h
+++ g_local.h
@@ -1521,6 +1521,7 @@ struct gclient_s {
     int            damage_blood;    // damage taken out of health
     int            damage_knockback; // impact damage
     vec3_t         damage_from;     // origin for vector calculation
+    int            damage_indicator_count;
 
     float          killer_yaw;      // when dead, look at killer
 
